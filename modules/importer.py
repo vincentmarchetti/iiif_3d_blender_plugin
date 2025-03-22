1709d27c5b41e1860cf897cc5b3e8afeb880227d
Vincent Marchetti <vmarchetti@kshell.com>
2025-03-22 07:06:39 -0400

    Refactored code that handled json variants into a separate 'utility' package

--- modules/importer.py
+++ modules/importer.py
@@ -13,90 +13,11 @@ from mathutils import Vector
 from .metadata import IIIFMetadata
 from .utils.color import hex_to_rgba
 from .utils.coordinates import Coordinates
+from .utils.json_patterns import force_as_object, force_as_singleton, force_as_list
 
 import logging
 logger = logging.getLogger("Import")
 
-def force_as_object( json_data , default_type=None):
-    """
-    argument
-    json_data : None, a python dict, or a StringProperty
-    
-    default_type : string, hint as to what type the resulting
-                    object will be
-                    
-    None and python dict are returned back
-    otherwise attempt, from the string value and default_type, if 
-    provided, a dict with id 
-    
-    
-    If string, and can be interprerted as a URI, will
-    be returned wrapped in a dict with that value as the id property
-    """
-    if json_data is None or type(json_data) == dict:
-        return json_data
-        
-    if type(json_data) == str:
-        # placeholder: at this point in code eventually will
-        # determine if json_data is a URI from which the resource
-        # can be rechieved by a remote query
-        determined_type = None
-        
-        _type = determined_type or default_type
-        retVal = {"id" : json_data}
-        
-        if _type:
-            retVal["type"] = _type
-            
-        return retVal
-            
-    
-    raise  ValueError("force_as_object : cannot interpret %r as a resource" % (json_data,))
-    
-    
-        
-def force_as_singleton( obj ):
-    """
-    A way of dealing with json-ld which is flexible
-    with regards to objects (python dictionaries) and lists
-    This function will follow rule:
-    If obj is None or an empty list : return None
-    if obj is a non-empty list:
-        return the 0th element, a report a warning message if some instances are being 
-            ignore
-    otherwist, return obj
-    
-    Use this if you expect value of property to be a single resource
-    """
-    
-    if obj is None or obj == []:
-        return None
-    if isinstance(obj,list):
-        if len(obj) != 1:
-            logger.warn("list of %i elements being coerced to singleton" % len(obj))
-        return obj[0]
-    return obj
-    
-def force_as_list( obj ):
-    """
-    A way of dealing with json-ld which is flexible
-    with regards to objects (python dictionaries) and lists
-    This function will follow rule:
-    If obj is None  return empty list
-    if obj is not a list: return [obj]
-    else return obj
-    
-    
-    Use this if you expect value of property to be a list of resources, possible empty
-    """
-    
-    if obj is None:
-        return []
-    if not isinstance(obj,list):
-        return [obj]
-    return obj
-    
-    
 class ImportIIIF3DManifest(Operator, ImportHelper):
     """Import IIIF 3D Manifest"""
 
