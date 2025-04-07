03bfa16a510b420cbeff3bf9c11a055d8872828c
Jamie Cope <jamie@gcope.com>
2025-04-07 12:05:43 -0400

    Refactor to work with a separate IIIF panel.

--- __init__.py
+++ __init__.py
@@ -8,7 +8,12 @@ from bpy.utils import register_class, unregister_class
 
 from .modules.exporter import ExportIIIF3DManifest
 from .modules.importer import ImportIIIF3DManifest
-from .modules.custom_props import AddIIIF3DObjProperties, AddIIIF3DCollProperties
+from .modules.custom_props import (
+    AddIIIF3DObjProperties,
+    AddIIIF3DCollProperties,
+    IIIF3DObjMetadataPanel,
+    IIIF3DCollMetadataPanel
+)
 from .modules.ui import (
     IIIFManifestPanel,
     register_ui_properties,
@@ -21,6 +26,8 @@ classes = (
     IIIFManifestPanel,
     AddIIIF3DObjProperties,
     AddIIIF3DCollProperties,
+    IIIF3DObjMetadataPanel,
+    IIIF3DCollMetadataPanel
 )
 
 def menu_func_import(self, context):
@@ -33,16 +40,6 @@ def menu_func_export(self, context):
         ExportIIIF3DManifest.bl_idname, text="IIIF 3D Manifest (.json)"
     )
 
-def btn_func_add_obj_props(self, context):
-    self.layout.operator(
-        AddIIIF3DObjProperties.bl_idname, text="Add IIIF3D Properties", icon='ADD'
-    )
-
-def btn_func_add_coll_props(self, context):
-    self.layout.operator(
-        AddIIIF3DCollProperties.bl_idname, text="Add IIIF3D Properties", icon='ADD'
-    )
-
 def register():
     for cls in classes:
         register_class(cls)
@@ -51,14 +48,10 @@ def register():
 
     TOPBAR_MT_file_import.append(menu_func_import)
     TOPBAR_MT_file_export.append(menu_func_export)
-    OBJECT_PT_custom_props.append(btn_func_add_obj_props)
-    COLLECTION_PT_collection_custom_props.append(btn_func_add_coll_props)
 
 def unregister():
     TOPBAR_MT_file_import.remove(menu_func_import)
     TOPBAR_MT_file_export.remove(menu_func_export)
-    OBJECT_PT_custom_props.remove(btn_func_add_obj_props)
-    COLLECTION_PT_collection_custom_props.remove(btn_func_add_coll_props)
 
     unregister_ui_properties()
 
--- modules/custom_props.py
+++ modules/custom_props.py
@@ -1,6 +1,8 @@
 from typing import Set
 import bpy
-from bpy.types import Collection, Context, Object, Operator
+from bpy.types import Collection, Context, Object, Operator, Panel
+
+### Metadata Operators ###
 
 class AddIIIF3DObjProperties(Operator):
     """Add IIIF3D manifest required object properties"""
@@ -11,10 +13,8 @@ class AddIIIF3DObjProperties(Operator):
     def execute(self, context: Context) -> Set[str]:
         try:
             obj = context.view_layer.objects.active
-            print(dir(obj))
-            #if not hasattr(obj, "annotation_id"):
-            if "annotation_id" not in obj:
-                obj["annotation_id"] = obj.name
+            if "iiif_annotation_id" not in obj:
+                obj["iiif_annotation_id"] = obj.name
             if "iiif_source_url" not in obj:
                 obj["iiif_source_url"] = "https://example.org/iiif/3d/manifest.json"
 
@@ -35,8 +35,8 @@ class AddIIIF3DCollProperties(Operator):
     def execute(self, context: Context) -> Set[str]:
         try:
             coll = context.collection
-            if "annotation_id" not in coll:
-                coll["annotation_id"] = coll.name
+            if "iiif_annotation_id" not in coll:
+                coll["iiif_annotation_id"] = coll.name
             if "iiif_source_url" not in coll:
                 coll["iiif_source_url"] = "https://example.org/iiif/3d/manifest.json"
 
@@ -47,3 +47,76 @@ class AddIIIF3DCollProperties(Operator):
             self.report({"ERROR"}, f"Error adding IIIF properties: {str(e)}")
             traceback.print_exc()
             return {"CANCELLED"}
+
+### Metadata Panels ###
+
+def rna_idprop_quote_path(prop):
+    return "[\"{:s}\"]".format(bpy.utils.escape_identifier(prop))
+
+def draw(self, context, context_member, prop_type):
+        
+    layout = self.layout
+
+    operator = "add_obj_properties.iiif_manifest" if context_member == "object" else "add_coll_properties.iiif_manifest"
+    obj = context.object if context_member == "object" else context.collection
+
+    row = layout.row()
+    props = row.operator(operator, text="Add Required", icon='ADD')
+    #props.data_path = context_member
+    del row
+    layout.separator()
+  
+    keys = obj.keys()
+    for key in obj.keys():
+        if not key.startswith("iiif_"):
+            continue
+        
+        split = layout.split(factor=0.4, align=True)
+        label_row = split.row()
+        label_row.alignment = 'RIGHT'
+        label_row.label(text=key, translate=False)
+
+        value_row = split.row(align=True)
+        value_column = value_row.column(align=True)
+        value_column.prop(obj, rna_idprop_quote_path(key), text="")
+
+        operator_row = value_row.row(align=True)
+        operator_row.alignment = 'RIGHT'
+        props = operator_row.operator("wm.properties_remove", text="", icon='X', emboss=False)
+        props.data_path = self._context_path
+        props.property_name = key
+
+class IIIF3DObjMetadataPanel(Panel):
+    """Add custom panel for manifest properties"""
+
+    bl_idname = "OBJECT_PT_iiif_obj_props"
+    bl_label = "IIIF Properties"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "object"
+    bl_options = {'DEFAULT_CLOSED'}
+    bl_order = 999
+
+    _context_path = "object"
+    _property_type = Object
+
+    def draw(self, context):
+        draw(self, context, self._context_path, self._property_type)
+
+class IIIF3DCollMetadataPanel(Panel):
+    """Add custom panel for manifest properties"""
+
+    bl_idname = "OBJECT_PT_iiif_coll_props"
+    bl_label = "IIIF Properties"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "collection"
+    bl_options = {'DEFAULT_CLOSED'}
+    bl_order = 999
+
+    _context_path = "collection"
+    _property_type = Collection
+
+    def draw(self, context):
+        draw(self, context, self._context_path, self._property_type)
+    
